4ae5982f55cfced680d166e75ece78847d049068
Andrey Bogdan <bezova@gmail.com>
2019-04-13 16:36:16 -0500

    plot_shap dependence and colormap

--- plotting.py
+++ plotting.py
@@ -11,6 +11,141 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from collections import OrderedDict
 import statsmodels.api as sm 
 
+from numpy import ma
+from matplotlib import cbook
+from matplotlib.colors import Normalize
+
+from matplotlib.colors import LinearSegmentedColormap
+
+#colormap from SHAP packakge
+red_blue = LinearSegmentedColormap('red_blue', { # #1E88E5 -> #ff0052
+    'red': ((0.0, 30./255, 30./255),
+            (1.0, 255./255, 255./255)),
+
+    'green': ((0.0, 136./255, 136./255),
+              (1.0, 13./255, 13./255)),
+
+    'blue': ((0.0, 229./255, 229./255),
+             (1.0, 87./255, 87./255)),
+
+    'alpha': ((0.0, 1, 1),
+              (0.5, 0.3, 0.3),
+              (1.0, 1, 1))
+})
+
+blue_green = LinearSegmentedColormap('red_blue', { # #1E88E5 -> #ff0052
+    'green': ((0.0, 30./255, 30./255),
+            (1.0, 255./255, 255./255)),
+
+    'red': ((0.0, 50./255, 50./255),
+              (1.0, 10./255, 10./255)),
+
+    'blue': ((0.0, 229./255, 229./255),
+             (1.0, 87./255, 87./255)),
+
+    'alpha': ((0.0, 1, 1),
+              (0.5, 0.3, 0.3),
+              (1.0, 1, 1))
+})
+
+# setting midpoint for colorbar
+# https://stackoverflow.com/questions/7404116/defining-the-midpoint-of-a-colormap-in-matplotlib
+class MidPointNorm(Normalize):    
+    def __init__(self, midpoint=0, vmin=None, vmax=None, clip=False):
+        Normalize.__init__(self,vmin, vmax, clip)
+        self.midpoint = midpoint
+
+    def __call__(self, value, clip=None):
+        if clip is None:
+            clip = self.clip
+
+        result, is_scalar = self.process_value(value)
+
+        self.autoscale_None(result)
+        vmin, vmax, midpoint = self.vmin, self.vmax, self.midpoint
+
+        if not (vmin < midpoint < vmax):
+            raise ValueError("midpoint must be between maxvalue and minvalue.")       
+        elif vmin == vmax:
+            result.fill(0) # Or should it be all masked? Or 0.5?
+        elif vmin > vmax:
+            raise ValueError("maxvalue must be bigger than minvalue")
+        else:
+            vmin = float(vmin)
+            vmax = float(vmax)
+            if clip:
+                mask = ma.getmask(result)
+                result = ma.array(np.clip(result.filled(vmax), vmin, vmax),
+                                  mask=mask)
+
+            # ma division is very slow; we can take a shortcut
+            resdat = result.data
+
+            #First scale to -1 to 1 range, than to from 0 to 1.
+            resdat -= midpoint            
+            resdat[resdat>0] /= abs(vmax - midpoint)            
+            resdat[resdat<0] /= abs(vmin - midpoint)
+
+            resdat /= 2.
+            resdat += 0.5
+            result = ma.array(resdat, mask=result.mask, copy=False)                
+
+        if is_scalar:
+            result = result[0]            
+        return result
+
+    def inverse(self, value):
+        if not self.scaled():
+            raise ValueError("Not invertible until scaled")
+        vmin, vmax, midpoint = self.vmin, self.vmax, self.midpoint
+
+        if cbook.iterable(value):
+            val = ma.asarray(value)
+            val = 2 * (val-0.5)  
+            val[val>0]  *= abs(vmax - midpoint)
+            val[val<0] *= abs(vmin - midpoint)
+            val += midpoint
+            return val
+        else:
+            val = 2 * (val - 0.5)
+            if val < 0: 
+                return  val*abs(vmin-midpoint) + midpoint
+            else:
+                return  val*abs(vmax-midpoint) + midpoint
+
+def plot_shap_dependence(shapVals_df, df, feature='ProppantIntensity_LBSPerFT', 
+                          feature_disp=None, cmap=plt.cm.coolwarm, s=10, title=None, color_bar=True, color_title=None):
+    feature_disp = feature if feature_disp is None else feature_disp
+    title = feature_disp if title is None else title
+    color_title = 'Feature Impact' if color_title is None else color_title
+    
+    x = df[feature].values
+    y = shapVals_df[feature].values
+    cvals =y
+    clow = np.nanpercentile(cvals, 5)
+    chigh = np.nanpercentile(cvals, 95)
+    norm = MidPointNorm(midpoint=0) if color_bar else MidPointNorm(midpoint=0, vmin=clow, vmax=chigh) # setting vmin/vmax will clip cbar
+#     scalarm = plt.cm.ScalarMappable(cmap=cmap, norm=norm)
+#     scalarm._A = []
+
+    cvals_nans = np.isnan(cvals)
+    cval_notNan = np.invert(cvals_nans)
+    
+    fig, ax = plt.subplots(figsize=(8,5))
+    ax.scatter(x[cvals_nans], y[cvals_nans], s=s, color="#777777", alpha=1, rasterized=len(x) > 500)
+    mapable = ax.scatter(x[cval_notNan], y[cval_notNan], s=s, c=cvals[cval_notNan], cmap=cmap, alpha=1,
+                    norm=norm, rasterized=len(x) > 500)
+    if color_bar: 
+        cb = colorbar(mapable, size=0.15)
+        cb.set_clim(clow, chigh) # setting vmin/vmaqx here will set even color beyond these numbers
+#         cb = colorbar(scalarm, size=0.15)
+        cb.set_label(color_title, size=13)
+        cb.outline.set_visible(False)
+        cb.set_alpha(1)
+    ax.set_xlabel(feature_disp, fontsize=14)
+    ax.set_ylabel('Feature Impact', fontsize=14)
+    ax.set_title(title, fontsize=14)
+
 def nan_to_mean(arr:np.ndarray, axis:int=0)->np.ndarray:
     '''fills nan with mean over axis .
     uses masked array to apply mean to complete nan columns np.nanmean() can not do that
